0ad8e17160d66f340e0db5cac2855381d18a678b
alfredaffia <alfredaffia478@gmail.com>
2025-11-26 14:26:12 +0100

    update conflict

--- server/src/assets/dto/list-marketplace.dto.ts
+++ server/src/assets/dto/list-marketplace.dto.ts
@@ -1,17 +1,11 @@
 import { IsNotEmpty, IsString, IsNumber, IsEthereumAddress } from 'class-validator';
 
 export class ListMarketplaceDto {
-<<<<<<< HEAD
-    assetId: string;
-    
-  @IsEthereumAddress()
-=======
   @IsString()
   @IsNotEmpty()
   assetId: string;
 
   @IsString()
->>>>>>> 0844788e83e739f1c56a49cfcf73347ed3ee11d4
   @IsNotEmpty()
   nftContract: string;
 
